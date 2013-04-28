96c00b867e1c3661d40a20f7ed992fb76b7ed254
Rob Lane <roblane09@gmail.com>
2013-04-28 16:05:44 -0700

    Initial make file and main.cpp file

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Rob Lane
+ *
+ * Created on April 28, 2013, 12:12 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+  return 0;
+}
+
